1a8bf1d5374e091d4c823528dbe2b4fedc87a750
gerrypang <watcher@aliyun.com>
2018-06-03 20:22:30 +0800

    代码生成器代码提交

---
+++ autocode/src/main/java/com/gerry/pang/model/EnumModel.java
@@ -0,0 +1,40 @@
+package com.gerry.pang.model;
+
+/**
+ * 枚举类-模型
+ * 
+ * @author pangguowei
+ * @version v 1.0.0 2018-06-01
+ */
+public class EnumModel {
+	/** 枚举英文名 */
+	private String codeName;
+	/** 枚举值 */
+	private String codeValue;
+	/** 中午解释 */
+	private String codeComment;
+
+	public String getCodeName() {
+		return codeName;
+	}
+
+	public void setCodeName(String codeName) {
+		this.codeName = codeName;
+	}
+
+	public String getCodeValue() {
+		return codeValue;
+	}
+
+	public void setCodeValue(String codeValue) {
+		this.codeValue = codeValue;
+	}
+
+	public String getCodeComment() {
+		return codeComment;
+	}
+
+	public void setCodeComment(String codeComment) {
+		this.codeComment = codeComment;
+	}
+}
