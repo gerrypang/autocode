bf4e07f3e0ab6049e2772883da30a0d32f0a3f1b
gerrypang <watcher@aliyun.com>
2018-06-03 20:19:48 +0800

    代码生成器代码提交

---
+++ autocode/src/main/java/com/gerry/pang/auto/Generate.java
@@ -0,0 +1,14 @@
+package com.gerry.pang.auto;
+
+public interface Generate {
+
+	public void generateEntityClass();
+	
+	public void generateDtoClass();
+	
+	public void generateEnumClass();
+	
+	public void generateAssemblerClass();
+	
+	public void generateHbm();
+}
